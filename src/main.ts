6de1a9457ad1c277aa0ad13da2488b86bfc97671
Boris Waaub <boris.waaub@gmail.com>
2022-05-05 16:39:27 +0200

    change

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
 import bodyParser from 'body-parser';
-import * as fs from 'fs';
-
-const httpsOptions = {
-  key: fs.readFileSync(
-    '/etc/letsencrypt/live/vps.digitalternative.be/fullchain.pem',
-  ),
-  cert: fs.readFileSync(
-    '/etc/letsencrypt/live/vps.digitalternative.be/privkey.pem ',
-  ),
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    httpsOptions,
     cors: {
       origin: true,
       credentials: true,
