e629a2a6fd367fbda4787c98df90c189ae91b2f2
Boris Waaub <boris.waaub@gmail.com>
2022-05-05 14:45:35 +0200

    add https

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
 import bodyParser from 'body-parser';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/fullchain.pem'),
+  cert: fs.readFileSync('./secrets/privkey.pem'),
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
+    httpsOptions,
     cors: {
       origin: true,
       credentials: true,
