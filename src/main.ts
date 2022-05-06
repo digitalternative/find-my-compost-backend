e9270e733dda9335783f2f4081cdd01959085867
Boris Waaub <boris.waaub@gmail.com>
2022-05-06 14:36:06 +0200

    add config nginx and change cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import session from 'express-session';
 import bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: true,
-      credentials: true,
-    },
-  });
+  // const app = await NestFactory.create(AppModule, {
+  //   cors: {
+  //     origin: true,
+  //     credentials: true,
+  //   },
+  // });
+  const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '3mb' }));
   app.use(bodyParser.urlencoded({ limit: '3mb', extended: true }));
   app.use(
@@ -21,7 +22,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
