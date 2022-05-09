6f6ffccb6d551b63257006fbf4717bbf1cc093f7
Boris Waaub <boris.waaub@gmail.com>
2022-05-09 10:45:09 +0200

    change front conf

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ mkdir mongodb
 mkdir mongodb/cfg_data
 mkdir mongodb/shard0_data
 sudo chown -R 1001:1001 mongodb
+sudo cp config/nginx/site-confs/default /opt/appdata/letsencrypt/config/nginx/site-confs/default
 docker-compose stop
 docker-compose rm
 docker image rm nestjs-api-prod:1.0.0
