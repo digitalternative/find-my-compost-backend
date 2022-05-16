2269c22d661668e01575950fb7f9527cb6524ec4
Boris Waaub <boris.waaub@gmail.com>
2022-05-16 12:39:33 +0200

    change mongosd

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 mkdir mongodb
 mkdir mongodb/cfg_data
 mkdir mongodb/shard0_data
-sudo chown -R 1001:1001 mongodb
-sudo chmod -R 777 mongodb
 sudo cp config/nginx/site-confs/default /opt/appdata/letsencrypt/config/nginx/site-confs/default
 docker-compose stop
 docker-compose rm
 docker image rm nestjs-api-prod:1.0.0
 docker system prune
+sudo chown -R 1001:1001 mongodb
+sudo chmod -R 777 mongodb
 docker-compose up -d
 docker logs find-my-compost-backend -f
